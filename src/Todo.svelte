ed3271edebc26a8307ce5beae705732424973983
Alper Çıtak <citak.alper@gmail.com>
2022-04-27 23:55:54 +0300

    transitions & cleanup

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,8 +1,28 @@
 <script>
     import { items } from "./stores.js";
     import { v4 as uuidv4 } from "uuid";
+    import { quintOut } from "svelte/easing";
+    import { crossfade } from "svelte/transition";
 
     let text;
+
+    const [send, receive] = crossfade({
+        duration: (d) => Math.sqrt(d * 200),
+
+        fallback(node, params) {
+            const style = getComputedStyle(node);
+            const transform = style.transform === "none" ? "" : style.transform;
+
+            return {
+                duration: 600,
+                easing: quintOut,
+                css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`,
+            };
+        },
+    });
 
     const handleAdd = () => {
         if (!text) return {};
@@ -14,25 +34,27 @@
                 text: text,
             },
         ];
+
         text = null;
     };
 
-    const onKeyPress = (e) => {
-        if (e.charCode === 13) handleAdd();
-    };
-    const handleRemove = (id) => {
-        $items = $items.filter((t) => t.id != id);
+    const handleRemove = (item) => {
+        $items = $items.filter((t) => t != item);
     };
 </script>
 
 <div>
-    <input bind:value={text} placeholder="todo" on:keypress={onKeyPress} />
+    <input
+        placeholder="todo"
+        bind:value={text}
+        on:keydown={(e) => e.key === "Enter" && handleAdd()}
+    />
     <button on:click={handleAdd} disabled={!text} title="Add">+</button>
 </div>
 
-<div>
-    {#each $items as item}
+{#each $items as item}
+    <div in:receive={{ key: item.id }} out:send={{ key: item.id }}>
         <input disabled value={item.text} />
-        <button on:click={handleRemove(item.id)} title="Remove">-</button>
-    {/each}
-</div>
+        <button on:click={handleRemove(item)} title="Remove">-</button>
+    </div>
+{/each}
